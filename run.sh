a3981c7b59a53ea01c5cac54724869843f33b5d4
hadwinzhy <hadwinzhy@gmail.com>
2017-01-08 22:10:35 +0800

    add notify for ubuntu

--- run.sh
+++ run.sh
@@ -2,6 +2,11 @@
 
 while true
 do
-   sleep 2
-   rake evcard:get_car_in_shop\['中信'\]
+   sleep 5
+   output=`rake evcard:get_car_in_shop\['中信'\]`
+   result=$(echo $output | grep "canRent")
+   echo $output
+   if [ "$result" != "" ]; then
+      /usr/bin/notify-send "发现可用车"
+   fi
 done
